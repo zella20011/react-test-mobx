c321a726ba9e42c27696697dc20dd93d514b22e3
Dima <zhyliak@gmail.com>
2023-10-12 23:13:56 +0300

    add locig to block and remove user

--- src/components/UserBlock/index.tsx
+++ src/components/UserBlock/index.tsx
@@ -6,24 +6,26 @@ import { UserType } from "../../type";
 //store
 import Users from '../../store/users';
 
-// import styled from "styled-components"
-
-// const Wrapper = styled.div``;
-
 const UserBlock: FC<{data: UserType}> = ({data}) => {
-  const [newName, setNewName] = useState<string>(data.name)
-  const [isBlocked, setIsBlocked] = useState<boolean>(false)
+  const [newName, setNewName] = useState<string>(data.name);
+  const [isBlocked, setIsBlocked] = useState<boolean>(false);
+
+  const handleChangeName = ({target: {value}}: {target: HTMLInputElement}) => {
+    if (isBlocked) return alert(`User ${newName} is Blocked`)
+
+    setNewName(value)
+  };
 
   const handlerDeleteUser = () => {
-    if (isBlocked) return alert(`User ${data.name} is Blocked`)
+    if (isBlocked) return alert(`User ${newName} is Blocked`)
 
     Users.removeUser(data.id)
-  }
+  };
 
   return (
     <div>
       <div>Name: {newName}</div>
-      <input type="text" onChange={({target: {value}}) => setNewName(value)} />
+      <input type="text" value={newName} onChange={handleChangeName} />
       <label htmlFor="isBlock">Is Blocked</label>
       <input type="checkbox" name="isBlock" id="isBlock" onChange={() => setIsBlocked((prev) => !prev)} />
       <button onClick={handlerDeleteUser}>Delete User</button>
